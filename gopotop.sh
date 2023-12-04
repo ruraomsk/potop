49406885c4bba3b2ed5a861979601bd0e7ed507e
Русинов Юрий <y.rusinov@kpda.ru>
2023-12-04 17:42:18 +0300

    Перенес программу в ТМР

--- gopotop.sh
+++ gopotop.sh
@@ -1,7 +1,12 @@
 #!/bin/ash
+mkdir /tmp/rura
+tar -xzvf potop.tar.gz -C "/tmp/rura"
+cp config.json /tmp/rura/
+cd /tmp/rura
 while true
 do
     echo "start potop" >> start
     ./potop > /dev/null 2>/dev/null
+    cp config.json /root
     echo "need restart " >> start
 done 
--- tokazan.bash
+++ tokazan.bash
@@ -6,6 +6,9 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 echo 'Copy potop to device Kazan'
-scp -P 222 potop root@185.27.195.194:/root 
+tar -czvf potop.tar.gz potop
+scp -P 222 potop.tar.gz root@185.27.195.194:/root 
+scp -P 222 gopotop.sh root@185.27.195.194:/root 
+
 #scp goirz.sh root@192.168.88.1:/root
 # scp rc.local root@192.168.88.1:/etc
